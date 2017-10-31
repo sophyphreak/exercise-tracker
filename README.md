58de4a293de32f7cb98f22a9dbc20f1eab7f75ae
Andrew Horn <arhorn@smcm.edu>
2017-10-31 22:04:47 +0700

    Finished tests for POST Exercise

--- README.md
+++ README.md
@@ -12,9 +12,11 @@ I can get an array of all users by getting api/exercise/users with the same info
 
 I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. App will return the user object with the exercise fields added.
 
-#### Not started:
+#### In progress:
 
 I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). App will return the user object with added array log and count (total exercise count).
+
+#### Not started:
 
 I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,3 @@
-// Create tests for POST Exercise, POST User, and GET Users
-
 const expect = require('expect');
 const moment = require('moment');
 const request = require('supertest');
@@ -66,7 +64,7 @@ describe('GET /api/exercise/users', () => {
 describe('POST /api/exercise/add', () => {
     it('should add an exercise with the given date', (done) => {
         const newExercise = {
-            userId: "59f86d76ee571c33c0357f33",
+            userId: users[0]._id.toHexString(),
             description: "Went swimming",
             duration: "30",
             date: "2017-10-1"
@@ -101,51 +99,61 @@ describe('POST /api/exercise/add', () => {
             });
     });
 
-    // it('should add an exercise with a date', (done) => {
-    //     const newExercise = {
-    //         userId: "59f81a4befb50c535cdc31e5",
-    //         description: "Went jogging",
-    //         duration: "20"
-    //     }
+    it('should add an exercise with a date', (done) => {
+        const newExercise = {
+            userId: users[0]._id.toHexString(),            
+            description: "Went jogging",
+            duration: "20"
+        }
 
-    //     request(app)
-    //         .post('/api/exercise/add')
-    //         .send(newExercise)
-    //         .expect(200)
-    //         .expect((res) => {
-    //             expect(res.body).toMatchObject(newExercise);
-    //             expect(res.body.date).toBeTruthy();
-    //         })
-    //         .end(async (err, res) => {
-    //             if (err) {
-    //                 done(err);
-    //             }
+        request(app)
+            .post('/api/exercise/add')
+            .send(newExercise)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.userId).toBe(newExercise.userId);
+                expect(res.body.description).toBe(newExercise.description);
+                expect(res.body.duration).toBe(parseInt(newExercise.duration));
+                expect(res.body.date).toBeTruthy();
+            })
+            .end(async (err, res) => {
+                if (err) {
+                    done(err);
+                }
 
-    //             try {
-    //                 const exercise = await Exercise.find({ description: newExercise.description });
-    //                 expect(exercise.length).toBe(1);
-    //                 expect(exercise[0]).toMatchObject(newExercise);
-    //                 expect(exercise[0].date).toBeTruthy();
-    //             } catch (e) {
-    //                 done(e);
-    //             }
-    //         });
-    // });
+                try {
+                    const exercise = await Exercise.find({ description: newExercise.description });
+                    expect(exercise.length).toBe(1);
+                    expect(exercise[0].userId).toBe(newExercise.userId);
+                    expect(exercise[0].description).toBe(newExercise.description);
+                    expect(exercise[0].duration).toBe(parseInt(newExercise.duration));
+                    expect(exercise[0].date).toBeTruthy();
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+    });
 
-    // it('should return an error for invalid exercise inputs', (done) => {
-    //     const newExercise = {
-    //         userId: "59f81a4befb50c535cdc31e5",
-    //         description: "",
-    //         duration: "20"
-    //     }
+    it('should return an error for invalid exercise inputs', (done) => {
+        const newExercise = {
+            userId: users[0]._id.toHexString(),                        
+            description: "thing thing thing",
+            duration: ""
+        }
 
-    //     request(app)
-    //         .post('/api/exercise/add')
-    //         .send(newExercise)
-    //         .expect(400)
-    //         .expect((res) => {
-    //             expect(res.body).toBeFalsy();
-    //         })
-    //         .end(done);
-    // });
+        request(app)
+            .post('/api/exercise/add')
+            .send(newExercise)
+            .expect(400)
+            .end( async (err, res) => {
+                try {
+                    const exercises = await Exercise.find({description: newExercise.description});
+                    expect(exercises.length).toBe(0);
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
+    });
 });
